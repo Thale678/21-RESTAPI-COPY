88626d21abe5093ed7bd3bf0419114518b72122f
Thale678 <manishthale69@gmail.com>
2025-04-01 20:47:09 +0530

    FDate

--- src/main/java/com/rest/api/controller/controller.java
+++ src/main/java/com/rest/api/controller/controller.java
@@ -11,7 +11,7 @@ public class controller {
 	@GetMapping("/test")
 	public String hello() {
 
-		return "Hello,Todays date is " + LocalDate.now().toString() + "\n Its my first jenkins pipeline";
+		return "Hello,Todays Date  is " + LocalDate.now().toString() + "\n Its my first jenkins pipeline";
 
 	}
 
